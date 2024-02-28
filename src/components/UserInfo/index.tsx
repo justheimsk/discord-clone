2821abdad68e7fb32b2d0d03bb07086ebf49396e
Henrique Müller Dimer <134540258+devdimer@users.noreply.github.com>
2024-02-28 16:21:29 +0000

    feat: added action buttons to userinfo component

--- src/components/UserInfo/index.tsx
+++ src/components/UserInfo/index.tsx
@@ -1,3 +1,4 @@
+import { FaCog, FaHeadphones, FaMicrophone } from "react-icons/fa";
 import RoundIcon from "../RoundIcon";
 import "./styles.scss";
 
@@ -6,11 +7,16 @@ export default function UserInfo() {
         <>
             <div id="user-info">
                 <div id="user-info__info">
-                    <RoundIcon status="dnd" small disableHover />
+                    <RoundIcon status="dnd" small disableHover imageURL="https://external-preview.redd.it/4PE-nlL_PdMD5PrFNLnjurHQ1QKPnCvg368LTDnfM-M.png?auto=webp&s=ff4c3fbc1cce1a1856cff36b5d2a40a6d02cc1c3" />
                     <div id="user-info__texts">
-                        <span>vonderheimsk</span>
+                        <span>Henrique</span>
                         <span id="user-info__status">#2890</span>
                     </div>
+                </div>
+                <div id="user-info__actions">
+                    <i><FaMicrophone /></i>
+                    <i><FaHeadphones /></i>
+                    <i><FaCog /></i>
                 </div>
             </div>
         </>
