1480357f3753b503f2c9678c0341f0aa487d4e96
Henrique Müller Dimer <134540258+devdimer@users.noreply.github.com>
2024-03-02 00:53:51 +0000

    feat: improved server class & added database manager class

---
+++ server/src/core/Database.ts
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose';
+
+export default class Database {
+    public connected: boolean = false;
+    private connectionTries: number = 0;
+
+    public init() {
+        // eslint-disable-next-line no-async-promise-executor
+        return new Promise(async (resolve) => {
+            mongoose.connection.on('connected', () => {
+                this.connected = true;
+                console.log('Database connected.');
+                resolve(true);
+            });
+
+            mongoose.connection.on('disconnected', async () => {
+                console.log('Database disconnected, reconnecting...');
+                this.connected = false;
+                this.connectionTries = 0;
+                await this.connect();
+            });
+
+            await this.connect();
+        });
+    }
+
+    private async connect() {
+        setTimeout(async () => {
+            if (!this.connected) {
+                if (this.connectionTries > 2) {
+                    console.log('Failed to connect database, exiting...');
+                    process.exit(1);
+                } else {
+                    this.connectionTries += 1;
+                    console.log(`Database is taking too long to connect, retrying... (${this.connectionTries})`);
+                    await this.connect();
+                }
+            }
+        }, 10000);
+
+        await mongoose.connect(process.env.DATABASE_URL as string);
+    }
+}
--- server/src/core/Server.ts
+++ server/src/core/Server.ts
@@ -1,6 +1,8 @@
 import Express from 'express';
 import { promises as fs } from 'node:fs';
-import { IBaseController } from './extends/BaseRoute';
+import { IBaseController } from './extends/BaseController';
+import Database from './Database';
+import bodyParser from 'body-parser';
 
 export default class Server {
     public app = Express();
@@ -8,9 +10,11 @@ export default class Server {
 
     public constructor(port = 8080) {
         this.port = port;
+        this.app.use(bodyParser.urlencoded({ extended: false }));
+        this.app.use(bodyParser.json());
     }
 
-    public async initRoutes() {
+    public async initControllers() {
         const routes = await fs.readdir('src/app/controllers');
         if (routes.length) {
             for (const TController of routes) {
@@ -18,15 +22,22 @@ export default class Server {
                 const controller: IBaseController = new Controller.default();
 
                 const router = Express.Router();
-                this.app.use('/', controller.init(router));
+                this.app.use(controller.path, controller.init(router));
             }
         }
+    }
+
+    public async initDatabase() {
+        const database = new Database();
+        await database.init();
     }
 
     public init() {
         // eslint-disable-next-line no-async-promise-executor
         return new Promise(async (resolve) => {
-            await this.initRoutes();
+            await this.initDatabase();
+            await this.initControllers();
+
             this.app.listen(this.port, () => {
                 resolve(this.port);
             });
