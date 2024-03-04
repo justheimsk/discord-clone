0f1294ac5c3cba201e795ecbc988c253807fc05a
Henrique Müller Dimer <henrique.mdimer@gmail.com>
2024-03-04 00:42:33 +0000

    feat: add common http response functions

---
+++ server/src/core/utils/HttpResponses.ts
@@ -0,0 +1,23 @@
+import { Response } from 'express';
+
+export default class HttpResponses {
+    public static Unauthorized(res: Response) {
+        return res.status(401).send('Unauthorized');
+    }
+
+    public static Conflict(res: Response) {
+        return res.status(409).send('Conflict');
+    }
+
+    public static InternalServerError(res: Response) {
+        return res.status(500).send('Internal Server Error');
+    }
+
+    public static NotFound(res: Response) {
+        return res.status(404).send('Not Found');
+    }
+
+    public static Ok(res: Response) {
+        return res.status(200).send('OK');
+    }
+}
