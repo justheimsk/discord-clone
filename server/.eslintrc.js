54c887b07b814390f62ea3cff98b62c8347213c3
Henrique Müller Dimer <henrique.mdimer@gmail.com>
2024-03-04 00:37:50 +0000

    chore: minor changes

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     },
     'extends': [
         'eslint:recommended',
-        'plugin:@typescript-eslint/recommended'
+        'plugin:@typescript-eslint/recommended',
     ],
     'overrides': [
         {
