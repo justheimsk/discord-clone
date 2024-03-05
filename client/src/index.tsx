6580fee6def94cbd3c8069b6f0d84006bebf0295
Henrique Müller Dimer <henrique.mdimer@gmail.com>
2024-03-05 01:16:16 +0000

    feat: add react router

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,13 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
-import App from './App';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import AuthPage from './pages/Auth';
+import ChatPage from './pages/Chat';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <ChatPage />
+  },
+  {
+    path: '/login',
+    element: <AuthPage method='login' />
+  },
+  {
+    path: '/register',
+    element: <AuthPage method='register' />
+  }
+]);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
