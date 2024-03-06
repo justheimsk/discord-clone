3f7307ece2ab0b3f3092bbef4395c70680ed1090
justheimsk <vonderheimsk@gmail.com>
2024-03-06 00:55:32 +0000

    feat: initialize the Wrapper lib & preloader

--- client/src/pages/Chat.tsx
+++ client/src/pages/Chat.tsx
@@ -1,8 +1,27 @@
+import { useEffect, useState } from 'react';
 import Layout from '../components/Layout';
+import client from '../lib/index';
+import PreLoader from '../components/PreLoader';
 
 export default function ChatPage() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      const token = localStorage.getItem('token');
+      if (!token) return window.location.replace('/login');
+
+      client.on('ready', () => {
+        setLoading(false);
+      });
+
+      await client.init(token);
+    })()
+  }, []);
+
   return (
     <>
+      <PreLoader active={loading} />
       <Layout />
     </>
   );
