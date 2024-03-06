a6bceb3544127cb44094cca81c85ba3e7905e4cc
justheimsk <vonderheimsk@gmail.com>
2024-03-06 00:54:04 +0000

    feat: display real user info

--- client/src/components/UserInfo/index.tsx
+++ client/src/components/UserInfo/index.tsx
@@ -1,16 +1,28 @@
 import { FaCog, FaHeadphones, FaMicrophone } from "react-icons/fa";
 import RoundIcon from "../RoundIcon";
 import "./styles.scss";
+import client from '../../lib/index';
+import { useEffect, useState } from "react";
 
 export default function UserInfo() {
+    const [username, setUsername] = useState('');
+    const [tag, setTag] = useState('');
+
+    useEffect(() => {
+        client.on('ready', () => {
+            setUsername(client.user?.username || '');
+            setTag(client.user?.tag || '');
+        })
+    }, []);
+
     return (
         <>
             <div id="user-info">
                 <div id="user-info__info">
                     <RoundIcon status="dnd" small disableHover imageURL="https://external-preview.redd.it/4PE-nlL_PdMD5PrFNLnjurHQ1QKPnCvg368LTDnfM-M.png?auto=webp&s=ff4c3fbc1cce1a1856cff36b5d2a40a6d02cc1c3" />
                     <div id="user-info__texts">
-                        <span>Henrique</span>
-                        <span id="user-info__status">#2890</span>
+                        <span>{username}</span>
+                        <span id="user-info__status">{tag}</span>
                     </div>
                 </div>
                 <div id="user-info__actions">
