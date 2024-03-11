16026bfe7037b62096574efa443bfef3176eb200
justheimsk <vonderheimsk@gmail.com>
2024-03-11 20:03:03 +0000

    feat: add BaseRoute middleware support

--- server/src/core/extends/BaseRoute.ts
+++ server/src/core/extends/BaseRoute.ts
@@ -1,15 +1,19 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Router } from 'express';
 
 export interface IBaseRoute {
     path: string;
+    middlewares?: any[];
 
     init: (router: Router) => Router;
 }
 
 export default class BaseRoute {
     public path: string;
+    public middlewares?: any[];
 
-    public constructor(path: string) {
+    public constructor(path: string, ...middlewares: any[]) {
         this.path = path;
+        this.middlewares = middlewares;
     }
 }
