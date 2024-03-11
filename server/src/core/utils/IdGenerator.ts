bb4291838982331d73e104b331ac345c8f2efc16
Henrique Müller Dimer <henrique.mdimer@gmail.com>
2024-03-11 20:03:19 +0000

    feat: add IdGenerator

---
+++ server/src/core/utils/IdGenerator.ts
@@ -0,0 +1,17 @@
+export default class IdGenerator {
+    static idLength = 18;
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    public static async generateId(model: any): Promise<string> {
+        let id = `${Date.now()}`;
+        for (let i = 0; i < (this.idLength - 13); i++) {
+            id += Math.floor(Math.random() * 9);
+        }
+
+        if (await model.findOne({ id: id })) {
+            return await this.generateId(model);
+        } else {
+            return id;
+        }
+    }
+}
