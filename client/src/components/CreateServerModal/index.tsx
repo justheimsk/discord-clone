01d41015eaf381d45ea98fa4747bfb1551de96ca
Henrique Müller Dimer <henrique.mdimer@gmail.com>
2024-03-12 00:50:06 +0000

    feat: add support to guild creation (not in server just UI)

--- client/src/components/CreateServerModal/index.tsx
+++ client/src/components/CreateServerModal/index.tsx
@@ -1,5 +1,8 @@
 import { FaAngleRight } from 'react-icons/fa';
 import './styles.scss';
+import { useState } from 'react';
+import client from '../../lib';
+import { FaCamera } from "react-icons/fa";
 
 interface IProps {
     active: boolean;
@@ -7,29 +10,65 @@ interface IProps {
 }
 
 export default function CreateServerModal(props: IProps) {
+    const [level, setLevel] = useState(0);
+
     return (
         <>
             <div onClick={() => props.setActive(false)} id="create-server-modal-overlay" className={`${props.active ? "create-server-modal-overlay--active" : ""}`}></div>
-            <div id="create-server-modal" className={`${props.active ? "create-server-modal--active" : ""}`}>
-                <div id="create-server-modal__text">
-                    <span id="create-server-modal__title">Create a server</span>
-                    <span id="create-server-modal__subtitle">Your server is where you and your friends hang out. Make yours and start talking</span>
-                </div>
-                <div id="create-server-modal__options">
-                    <div className="create-server-modal__option">
-                        <div className="create-server-modal__option-text">
-                            <span>Create My Own</span>
+            <div id="create-server-modal" className={`${props.active ? "create-server-modal--active" : ""} ${level === 0 ? "" : level === 1 ? "create-server-modal--invite" : level === 2 ? "create-server-modal--create" : ""}`}>
+                <div id="create-server-modal__first-prompt" className="create-server-modal__column">
+                    <div className="create-server-modal__text">
+                        <span className="create-server-modal__title">Create a server</span>
+                        <span className="create-server-modal__subtitle">Your server is where you and your friends hang out. Make yours and start talking</span>
+                    </div>
+                    <div id="create-server-modal__options">
+                        <div onClick={() => setLevel(2)} className="create-server-modal__option">
+                            <div className="create-server-modal__option-text">
+                                <span>Create My Own</span>
+                            </div>
+                            <i className="create-server-modal__option-icon"><FaAngleRight /></i>
                         </div>
-                        <i className="create-server-modal__option-icon"><FaAngleRight /></i>
+                    </div>
+                    <div id="create-server-modal__footer">
+                        <div id="create-server-modal__footer-title">
+                            <span>Have an invite already?</span>
+                        </div>
+                        <button onClick={() => setLevel(1)} id="create-server-modal__footer-button">
+                            Join a server
+                        </button>
                     </div>
                 </div>
-                <div id="create-server-modal__footer">
-                    <div id="create-server-modal__footer-title">
-                        <span>Have an invite already?</span>
+                <div id="create-server-modal__invite" className="create-server-modal__column">
+                    <div className="create-server-modal__text">
+                        <span className="create-server-modal__title">Join a server</span>
+                        <span className="create-server-modal__subtitle">Type a invite link below to join a server</span>
                     </div>
-                    <button id="create-server-modal__footer-button">
-                        Join a server
-                    </button>
+                    <div className="create-server-modal__input-control">
+                        <label htmlFor="" className="create-server-modal__input-label">Invite link</label>
+                        <input type="text" placeholder='https://discord-clone-nine-eta.vercel.app/invites/hJks87I' className="create-server-modal__input" />
+                    </div>
+                    <div className="create-server-modal__footer">
+                        <button onClick={() => setLevel(0)} className="create-server-moval__goback">Go back</button>
+                        <button className="create-server-modal__button">Join server</button>
+                    </div>
+                </div>
+                <div id="create-server-modal__create" className="create-server-modal__column">
+                    <div className="create-server-modal__text">
+                        <span className="create-server-modal__title">Create a server</span>
+                        <span className="create-server-modal__subtitle">Your server is where you and your friends hang out. Make yours and start talking</span>
+                    </div>
+                    <div id="create-server-modal__server-icon">
+                        <i><FaCamera /></i>
+                        <span>Send</span>
+                    </div>
+                    <div className="create-server-modal__input-control">
+                        <label htmlFor="" className="create-server-modal__input-label">Server name</label>
+                        <input type="text" placeholder={`${client.user?.username}'s server`} className="create-server-modal__input" />
+                    </div>
+                    <div className="create-server-modal__footer">
+                        <button onClick={() => setLevel(0)} className="create-server-moval__goback">Go back</button>
+                        <button className="create-server-modal__button">Create server</button>
+                    </div>
                 </div>
             </div>
         </>
