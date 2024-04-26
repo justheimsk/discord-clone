d63340e3e03969318ec3ef14dc2d68e109426820
justheimsk <vonderheimsk@gmail.com>
2024-04-26 14:00:08 +0000

    fix: fix vulnerability

--- server/src/core/middlewares/Auth.ts
+++ server/src/core/middlewares/Auth.ts
@@ -1,14 +1,17 @@
 import { NextFunction, Request, Response } from 'express';
 import HttpResponses from '../utils/HttpResponses';
 import AuthService from '../../app/services/auth.service';
+import UserService from '../../app/services/user.service';
 
-export default function AuthMiddleware(req: Request, res: Response, next: NextFunction) {
+export default async function AuthMiddleware(req: Request, res: Response, next: NextFunction) {
     const token = req.headers.authorization;
     if (!token) return HttpResponses.Unauthorized(res);
 
     const authService = new AuthService();
     const id = authService.verifyToken(token);
     if (!id) return HttpResponses.Unauthorized(res);
+    const userService = new UserService();
+    if (!(await userService.find({ id }))) return HttpResponses.Unauthorized(res);
 
     res.locals.id = id;
     next();
