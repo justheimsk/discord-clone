30611104fe4dbbca1fb036de0a6fa398d49d824b
justheimsk <vonderheimsk@gmail.com>
2024-04-26 13:48:13 +0000

    feat: update user controller & add new features to guild controller

--- server/src/app/controllers/guild.controller.ts
+++ server/src/app/controllers/guild.controller.ts
@@ -4,19 +4,25 @@ import { plainToClass } from 'class-transformer';
 import GuildCreateBody from '../dtos/guildCreate.dto';
 import GuildService from '../services/guild.service';
 import HttpResponses from '../../core/utils/HttpResponses';
+import ChannelCreateBody from '../dtos/channelCreate.dto';
+import ChannelService from '../services/channel.service';
 
 export default class GuildController extends BaseController {
-    public constructor(private readonly guildService = new GuildService()) {
+    public constructor(private readonly guildService = new GuildService(), private readonly channelService = new ChannelService()) {
         super();
 
         this.create = this.create.bind(this);
-        this.findMyGuilds = this.findMyGuilds.bind(this);
+        this.join = this.join.bind(this);
+        this.getMembers = this.getMembers.bind(this);
+        this.createChannel = this.createChannel.bind(this);
+        this.getChannels = this.getChannels.bind(this);
     }
 
     public async create(req: Request, res: Response) {
         try {
             const body = plainToClass(GuildCreateBody, req.body);
             const id = await this.guildService.create(body, res.locals.id);
+            await this.guildService.join(res.locals.id, id);
 
             res.status(201).send({
                 id
@@ -31,15 +37,70 @@ export default class GuildController extends BaseController {
         }
     }
 
-    public async findMyGuilds(req: Request, res: Response) {
-        try {
-            const guilds = await this.guildService.findMany({ ownerId: res.locals.id });
+    public async join(req: Request, res: Response) {
+        if (!res.locals.id) return HttpResponses.Unauthorized(res);
 
-            res.status(200).send({
-                guilds: guilds || [],
-            });
+        try {
+            const id = req.params.id;
+            if (!id) return HttpResponses.BadRequest(res);
+
+            await this.guildService.join(res.locals.id, id);
+            return HttpResponses.Ok(res);
         } catch (err) {
-            console.log('Failed to fecth user guilds', err);
+            console.log('Failed to join guild: ', err);
+            return HttpResponses.InternalServerError(res);
+        }
+    }
+
+    public async getMembers(req: Request, res: Response) {
+        try {
+            if (!res.locals.id) return HttpResponses.Unauthorized(res);
+
+            const id = req.params.id;
+            if (!id) return HttpResponses.BadRequest(res);
+
+            const members = await this.guildService.getMembers(id);
+            if (!members.find((m) => m.id == res.locals.id)) return HttpResponses.Unauthorized(res);
+
+            return res.status(200).send({ members });
+        } catch (err) {
+            console.log('Failed to get guild members: ', err);
+            return HttpResponses.InternalServerError(res);
+        }
+    }
+
+    public async createChannel(req: Request, res: Response) {
+        try {
+            if (!res.locals.id) return HttpResponses.Unauthorized(res);
+
+            const guildId = req.params.id;
+            if (!guildId) return HttpResponses.BadRequest(res);
+            if (!(await this.guildService.userIsMemberFromGuild(res.locals.id, guildId))) return HttpResponses.Unauthorized(res);
+
+            let body = req.body;
+            if (!body) return HttpResponses.BadRequest(res);
+            body = plainToClass(ChannelCreateBody, body);
+
+            const id = await this.channelService.create(body, guildId);
+            return res.status(201).send({ id });
+        } catch (err) {
+            console.log('Failed to create channel: ', err);
+            return HttpResponses.InternalServerError(res);
+        }
+    }
+
+    public async getChannels(req: Request, res: Response) {
+        try {
+            if (!res.locals.id) return HttpResponses.Unauthorized(res);
+
+            const guildId = req.params.id;
+            if (!guildId) return HttpResponses.BadRequest(res);
+            if (!(await this.guildService.userIsMemberFromGuild(res.locals.id, guildId))) return HttpResponses.Unauthorized(res);
+
+            const channels = await this.channelService.getChannels(guildId);
+            return res.status(200).send({ channels });
+        } catch (err) {
+            console.log('Failed to get guild channels: ', err);
             return HttpResponses.InternalServerError(res);
         }
     }
--- server/src/app/controllers/user.controller.ts
+++ server/src/app/controllers/user.controller.ts
@@ -3,15 +3,18 @@ import UserService from '../services/user.service';
 import BaseController from '../../core/extends/BaseController';
 import AuthService from '../services/auth.service';
 import HttpResponses from '../../core/utils/HttpResponses';
+import GuildService from '../services/guild.service';
 
 export default class UserController extends BaseController {
     public constructor(
         private readonly userService = new UserService(),
-        private readonly authService = new AuthService()
+        private readonly guildService = new GuildService()
     ) {
         super();
 
         this.getMe = this.getMe.bind(this);
+        this.ownedGuilds = this.ownedGuilds.bind(this)
+        this.getGuilds = this.getGuilds.bind(this);
     }
 
     public async getMe(req: Request, res: Response) {
@@ -19,7 +22,7 @@ export default class UserController extends BaseController {
             if (!res.locals.id) return HttpResponses.Unauthorized(res);
 
             const user = await this.userService.find({ id: res.locals.id }, '-password');
-            if (!user) return HttpResponses.NotFound(res);
+            if (!user) return HttpResponses.Unauthorized(res);
 
             res.status(200).send(user);
         } catch (err) {
@@ -27,4 +30,32 @@ export default class UserController extends BaseController {
             return HttpResponses.InternalServerError(res);
         }
     }
+
+    public async getGuilds(req: Request, res: Response) {
+        try {
+            if (!res.locals.id) return HttpResponses.Unauthorized(res);
+
+            const guilds = await this.userService.getGuilds(res.locals.id);
+            return res.status(200).send({
+                guilds: guilds || []
+            });
+        } catch (err) {
+            console.log('Failed to get user guilds: ', err);
+            return HttpResponses.InternalServerError(res);
+        }
+    }
+
+    public async ownedGuilds(req: Request, res: Response) {
+        try {
+            const guilds = await this.guildService.findMany({ ownerId: res.locals.id });
+
+            res.status(200).send({
+                guilds: guilds || [],
+            });
+        } catch (err) {
+            console.log('Failed to fecth user guilds', err);
+            return HttpResponses.InternalServerError(res);
+        }
+    }
+
 }
