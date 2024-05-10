368190d95b879b156ece41c5d404de72edcbe9ff
justheimsk <vonderheimsk@gmail.com>
2024-05-10 17:31:49 +0000

    refactor: minor changes

--- client/src/components/Role/index.tsx
+++ client/src/components/Role/index.tsx
@@ -10,7 +10,7 @@ export default function Role(props: IProps) {
     return (
         <>
             <div className="role">
-                <div className="role__name">Available - 1</div>
+                <div className="role__name">Available - {props.members.length || 0}</div>
                 <div className="role__users">
                     {props.members.map((member, index) => (
                         <User key={index} username={member.user.username} />
