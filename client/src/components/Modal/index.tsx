c999690f2f359ef1e885cd89417ac9b08d39b738
Henrique <134540258+devdimer@users.noreply.github.com>
2024-05-10 17:34:18 +0000

    feat: add close button to modal

--- client/src/components/Modal/index.tsx
+++ client/src/components/Modal/index.tsx
@@ -1,10 +1,12 @@
-import { HTMLAttributes, ReactNode } from "react";
+import { Dispatch, HTMLAttributes, ReactNode, SetStateAction, useState } from "react";
 import Button from "../Button";
 import "./styles.scss";
+import { FaTimes } from "react-icons/fa";
 
 interface IProps extends HTMLAttributes<HTMLDivElement> {
   active: boolean;
-  setActive: any;
+  onClose: () => void;
+  onSuccess?: () => void;
   children: ReactNode;
 }
 
@@ -12,15 +14,16 @@ export default function Modal(props: IProps) {
   return (
     <>
       <div className={`${props.active ? 'modal__active' : ''} modal`}>
+        <i onClick={() => props.onClose()} className="modal--close"><FaTimes /></i>
         <div className="modal--content">
           {props.children}
         </div>
         <div className="modal--footer">
-          <Button onClick={() => props.setActive(false)} label='Cancelar' sstyle='outline' />
-          <Button label="Criar canal" />
+          <Button onClick={() => props.onClose()} label='Cancelar' sstyle='outline' />
+          <Button onClick={() => props.onSuccess?.()} label="Criar canal" />
         </div>
       </div>
-      <div onClick={() => props.setActive(false)} className={`${props.active ? 'modal-overlay__active' : ''} modal-overlay`}></div>
+      <div onClick={() => props.onClose()} className={`${props.active ? 'modal-overlay__active' : ''} modal-overlay`}></div>
     </>
   )
 }
