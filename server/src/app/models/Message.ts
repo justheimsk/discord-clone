d26cec953cfbbfc4482847c42326d2912c13c392
justheimsk <vonderheimsk@gmail.com>
2024-05-20 01:45:30 +0000

    chore: add Message model

---
+++ server/src/app/models/Message.ts
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const Schema = new mongoose.Schema({
+  id: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  guildId: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  channelId: {
+    type: String,
+    required: true
+  },
+  authorId: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: 'GuildMember',
+    required: true,
+  }
+}, { timestamps: true });
+
+export default mongoose.model('Message', Schema);
