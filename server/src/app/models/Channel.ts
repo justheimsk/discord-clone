fdc14659d9291d96da537844c525fd2f2dd3773e
devdimer <henrique.mdimer@gmail.com>
2024-05-20 01:45:18 +0000

    chore: add fields

--- server/src/app/models/Channel.ts
+++ server/src/app/models/Channel.ts
@@ -6,6 +6,14 @@ const Schema = new mongoose.Schema({
     required: true,
     unique: true,
   },
+  type: {
+    type: Number,
+    required: true
+  },
+  parentId: {
+    type: String,
+    required: false
+  },
   guildId: {
     type: String,
     required: true,
