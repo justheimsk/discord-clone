8490c42ac823026f3ecc05884a5a04009ffa03bb
justheimsk <vonderheimsk@gmail.com>
2024-05-20 01:32:54 +0000

    feat: add selected state to channels

--- client/src/components/Channel/index.tsx
+++ client/src/components/Channel/index.tsx
@@ -1,15 +1,18 @@
 import { FaCog, FaUserPlus } from "react-icons/fa";
 import { HiHashtag } from 'react-icons/hi';
+import client from "../../lib";
 import "./styles.scss";
 
 export interface IProps {
     name: string;
+    id: string;
+    selected?: boolean;
 }
 
 export default function Channel(props: IProps) {
     return (
         <>
-            <div className="channel">
+            <div onClick={() => client.selectChannel(props.id)} className={`channel ${props.selected ? 'channel--selected' : ''}`}>
                 <div className="channel__infos">
                     <div className="channel__icon"><HiHashtag /></div>
                     <div className="channel__name">{props.name}</div>
