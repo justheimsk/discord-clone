85dd0c886a73a6a69ef4700c2c096458b5a0cbf7
devdimer <henrique.mdimer@gmail.com>
2024-05-20 01:40:09 +0000

    feat: add fields & basic crud operations

--- client/src/lib/core/classes/Channel.ts
+++ client/src/lib/core/classes/Channel.ts
@@ -1,5 +1,6 @@
 import Client from "../Client";
 import { Guild } from "./Guild";
+import Message from "./Message";
 
 export class Channel {
   private client: Client;
@@ -7,6 +8,10 @@ export class Channel {
   public id: string;
   public guildId: string;
   public guild: Guild;
+  public type: number;
+  public parentId?: string;
+  public messages: Message[] = [];
+  public loaded: boolean = false;
 
   public constructor(data: any, client: Client) {
     if (!data || !data.id || !data.name || !client) throw new Error('Invalid or missing data');
@@ -16,5 +21,44 @@ export class Channel {
     this.id = data.id;
     this.guildId = data.guildId;
     this.guild = new Guild(data.guild, client);
+    this.type = data.type;
+    this.parentId = data.parentId;
+  }
+
+  public async load() {
+    try {
+      await this.loadMessages();
+      this.loaded = true;
+    } catch (err) {
+      console.log(err);
+    }
+    return true;
+  }
+
+  public async loadMessages() {
+    const res = await this.client.rest.request('get', `/channels/${this.id}/messages`, null, true);
+
+    if (res && res.data && res.data.messages) {
+      for (const msg of res.data.messages) {
+        this.messages.push(new Message(msg, this.client));
+      }
+    }
+
+    return true;
+  }
+
+  public pushMessage(data: any) {
+    try {
+      this.messages.push(new Message(data, this.client));
+
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  public async createMessage(content: string) {
+    return await this.client.rest.request('post', `/channels/${this.id}/messages`, {
+      content
+    }, true);
   }
 }
