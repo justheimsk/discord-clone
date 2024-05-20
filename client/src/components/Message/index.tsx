38d9836d8d5efbf642511bf775a5fd7e88885984
justheimsk <vonderheimsk@gmail.com>
2024-05-20 01:36:34 +0000

    feat: messages now can be stacked

--- client/src/components/Message/index.tsx
+++ client/src/components/Message/index.tsx
@@ -1,14 +1,24 @@
+import LibMessage from "../../lib/core/classes/Message";
 import RoundIcon from "../RoundIcon";
 import "./styles.scss";
 
-export default function Message() {
+interface IProps {
+    content: string;
+    username: string;
+    attached?: LibMessage[]
+}
+
+export default function Message(props: IProps) {
     return (
         <>
             <div className="message">
                 <RoundIcon medium disableHover imageURL="https://external-preview.redd.it/4PE-nlL_PdMD5PrFNLnjurHQ1QKPnCvg368LTDnfM-M.png?auto=webp&s=ff4c3fbc1cce1a1856cff36b5d2a40a6d02cc1c3" />
                 <div className="message__data">
-                    <span className="message__author">Henrique</span>
-                    <span className="message__content">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore id consectetur officia accusamus explicabo eveniet beatae nostrum facere numquam. Magnam aperiam dolor suscipit! Ratione aut, perferendis voluptatem enim tenetur optio.</span>
+                    <span className="message__author">{props.username}</span>
+                    {!props.attached?.length && <span className="message__content">{props.content}</span>}
+                    {props.attached?.map((msg) => (
+                        <span className="message__content">{msg.content}</span>
+                    ))}
                 </div>
             </div>
         </>
