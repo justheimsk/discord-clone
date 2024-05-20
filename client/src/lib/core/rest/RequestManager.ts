b15da444fcfed30cea6933801acadb78e0fba776
justheimsk <vonderheimsk@gmail.com>
2024-05-20 01:41:19 +0000

    chore: better options

--- client/src/lib/core/rest/RequestManager.ts
+++ client/src/lib/core/rest/RequestManager.ts
@@ -3,14 +3,20 @@ import axios, { AxiosInstance, AxiosResponse } from 'axios';
 
 export type METHODS = 'get' | 'post' | 'patch' | 'delete';
 
+export interface RequestManagerOptions {
+    url: string;
+    port?: string;
+    secure?: boolean;
+}
+
 export default class RequestManager {
     public client: Client;
     private instance: AxiosInstance;
     public baseUrl: string;
 
-    public constructor(client: Client, url: string) {
+    public constructor(client: Client, options: RequestManagerOptions) {
         this.client = client;
-        this.baseUrl = url;
+        this.baseUrl = options.secure ? 'https://' : 'http://' + options.url + `:${options.port || 8080}`;
         this.instance = axios.create({
             baseURL: this.baseUrl
         });
