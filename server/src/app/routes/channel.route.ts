1dd8577095c1b23b5e1e5027aea7e8047f25048e
devdimer <henrique.mdimer@gmail.com>
2024-05-20 01:46:32 +0000

    feat: add channel operation routes

--- server/src/app/routes/channel.route.ts
+++ server/src/app/routes/channel.route.ts
@@ -1,15 +1,16 @@
 import { Router } from "express";
 import BaseRoute, { IBaseRoute } from "../../core/extends/BaseRoute";
 import AuthMiddleware from "../../core/middlewares/Auth";
-
+import ChannelController from "../controllers/channel.controller";
 
 export default class ChannelRoute extends BaseRoute implements IBaseRoute {
-  public constructor() {
+  public constructor(public channelController = new ChannelController()) {
     super('/channels', AuthMiddleware);
   }
 
   public init(router: Router) {
-    router.post('/');
+    router.post('/:id/messages', this.channelController.createMessage);
+    router.get('/:id/messages', this.channelController.getMessages);
     return router;
   }
 }
