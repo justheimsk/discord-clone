141f3013871dd9bbdc16a550c9c35ea09c9d7c35
devdimer <henrique.mdimer@gmail.com>
2024-05-20 01:34:48 +0000

    chore: improve selection handler & better channel types handling

--- client/src/components/ChannelList/index.tsx
+++ client/src/components/ChannelList/index.tsx
@@ -1,19 +1,27 @@
 import { FormEvent, useEffect, useState } from "react";
 import { FaHashtag, FaHeadphones } from "react-icons/fa";
 import client from "../../lib";
-import { Channel } from "../../lib/core/classes/Channel";
+import { Channel as LibChannel } from "../../lib/core/classes/Channel";
 import Category from "../Category";
+import Channel from "../Channel";
 import Input from "../Input";
 import Modal from "../Modal";
 import "./styles.scss";
 
 export default function ChannelList() {
-    const [channels, setChannels] = useState<Channel[]>([]);
+    const [channels, setChannels] = useState<LibChannel[]>([]);
     const [createModal, setCreateModal] = useState(false);
     const [type, setType] = useState(99);
     const [name, setName] = useState('');
+    const [parent, setParent] = useState('');
+    const [selected, setSelected] = useState<LibChannel | undefined>();
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
+        client.on('selectChannel', () => {
+            setSelected(client.selectedChannel);
+        })
+
         client.on('selectGuild', async () => {
             const guild = client.selectedGuild;
             if (guild) {
@@ -21,11 +29,7 @@ export default function ChannelList() {
                     setChannels(guild.channels);
                 });
 
-                if (guild.channels.length) setChannels(guild.channels);
-                else {
-                    await guild.getChannels();
-                    if (client.selectedGuild?.id === guild.id) setChannels(guild.channels);
-                }
+                setChannels(guild.channels);
             }
         });
     }, []);
@@ -34,49 +38,94 @@ export default function ChannelList() {
         setCreateModal(false);
         setType(99);
         setName('');
+        setParent('');
     }
 
     async function createChannel() {
         const guild = client.selectedGuild;
 
-        if(guild) {
-            await guild.createChannel(name);
+        if (guild) {
+            try {
+                const id = await guild.createChannel(name, type, parent);
+                // client.selectChannel(id);
+            } catch (err) {
+                setLoading(false);
+                return;
+            }
         }
 
         closeModal();
     }
 
+    function openModal(id: string) {
+        setParent(id);
+        setCreateModal(true);
+    }
+
     return (
         <>
-            <Modal id="channel-list--modal" onSuccess={createChannel} active={createModal} onClose={closeModal}>
+            <Modal disabled={type == 99} loading={loading} setLoading={setLoading} id="channel-list--modal" onSuccess={createChannel} active={createModal} onClose={closeModal}>
                 <h4 id="channel-list--modal-title">Create channel</h4>
                 <span className="channel-list--modal-subtitle">Channel type</span>
 
                 <div id="channel-list--modal-channel-types">
-                    <div onClick={() => setType(0)} className={`channel-type ${type == 0 ? 'channel-type__active' : ''}`}>
-                        <i><FaHashtag /></i>
-                        <div className="channel-type--infos">
-                            <h5>Text</h5>
-                            <span>Envie mensagens, imagens, GIFs, emojis, opiniões e piadas</span>
-                        </div>
-                        <input checked={type == 0} type="radio" />
-                    </div>
-                    <div className={`channel-type channel-type__disabled ${type == 1 ? 'channel-type__active' : ''}`}>
-                        <i><FaHeadphones /></i>
-                        <div className="channel-type--infos">
-                            <h5>Voice</h5>
-                            <span>Passe o tempo com a turma com voz, video e compartilhamento de tela</span>
-                        </div>
-                        <input checked={type == 1} type="radio" />
-                    </div>
+                    <ChannelType
+                        type={0}
+                        setType={setType}
+                        selected={type == 0}
+                        title="Categoria"
+                        description="Agrupe e organize canais e permissões"
+                    />
+                    <ChannelType
+                        type={1}
+                        setType={setType}
+                        selected={type == 1}
+                        title="Texto"
+                        description="Envie mensagens, imagens, GIFs, emojis, opiniões e piadas"
+                    />
+                    <ChannelType
+                        type={2}
+                        setType={setType}
+                        selected={type == 2}
+                        title="Voz"
+                        description="Passe o tempo com a turma com voz, video e compartilhamento de tela"
+                        disabled
+                    />
                     <span className="channel-list--modal-subtitle">Nome do canal</span>
                     {/* @ts-ignore */}
                     <Input onChange={(e) => setName(e.target.value)} placeholder="chat-geral" />
                 </div>
             </Modal>
             <div id="channel-list">
-                <Category name="main" channels={channels} createModal={setCreateModal} />
+                {channels.filter((c) => !c.parentId && c.type != 0).map((channel) => (
+                    <Channel id={channel.id} selected={selected?.id == channel.id} name={channel.name} key={channel.id} />
+                ))}
+                {channels.map((channel) => (
+                    channel.type == 0 && <Category selectedChannel={selected} name={channel.name} id={channel.id} createModal={openModal} key={channel.id} channels={channels.filter((c) => c.parentId == channel.id)} />
+                ))}
             </div>
         </>
+    )
+}
+
+interface ChannelTypeIProps {
+    selected: boolean;
+    type: number;
+    setType: any;
+    title: string;
+    description: string;
+    disabled?: boolean;
+}
+
+export function ChannelType(props: ChannelTypeIProps) {
+    return (
+        <div onClick={() => props.disabled ? {} : props.setType(props.type)} className={`channel-type ${props.selected ? 'channel-type__active' : ''} ${props.disabled ? "channel-type__disabled" : ""}`}>
+            <i><FaHashtag /></i>
+            <div className="channel-type--infos">
+                <h5>{props.title}</h5>
+                <span>{props.description}</span>
+            </div>
+            <input checked={props.selected} type="radio" />
+        </div>
     )
 }
