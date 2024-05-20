9bab09dec201d5c7468be2589b85dd585ffd1fed
devdimer <henrique.mdimer@gmail.com>
2024-05-20 01:44:52 +0000

    chore: add fields

--- server/src/app/dtos/channelCreate.dto.ts
+++ server/src/app/dtos/channelCreate.dto.ts
@@ -1,8 +1,15 @@
-import { IsNotEmpty, Length } from "class-validator";
+import { IsNotEmpty, Length, IsNumber, IsOptional } from "class-validator";
 
 export default class ChannelCreateBody {
 
   @Length(1, 20)
   @IsNotEmpty()
   public name!: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  public type!: number;
+
+  @IsOptional()
+  public parentId?: string;
 }
