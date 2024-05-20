4747178409a352f863c321374deddf3ec1873254
devdimer <henrique.mdimer@gmail.com>
2024-05-20 01:47:46 +0000

    chore: init gateway server

--- server/src/core/Server.ts
+++ server/src/core/Server.ts
@@ -4,6 +4,7 @@ import Database from './Database';
 import bodyParser from 'body-parser';
 import { IBaseRoute } from './extends/BaseRoute';
 import cors from 'cors';
+import Gateway from './Websocket';
 
 export default class Server {
     public app = Express();
@@ -38,6 +39,11 @@ export default class Server {
         await database.init();
     }
 
+    public initWebsocket() {
+        Gateway.init();
+        console.log(`Gateway running on port: ${Gateway.port}`);
+    }
+
     public initMiddlewares() {
         this.app.use(bodyParser.urlencoded({ extended: false }));
         this.app.use(bodyParser.json());
@@ -51,6 +57,7 @@ export default class Server {
         return new Promise(async (resolve) => {
             this.initMiddlewares();
             await this.initDatabase();
+            this.initWebsocket();
             await this.initRoutes();
 
             this.app.listen(this.port, () => {
