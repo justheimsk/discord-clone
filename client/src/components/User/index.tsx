10f500b2875d3ed56bfb279b29ed6cd7730189be
Henrique <134540258+devdimer@users.noreply.github.com>
2024-06-08 23:14:07 +0000

    feat: display status

--- client/src/components/User/index.tsx
+++ client/src/components/User/index.tsx
@@ -3,13 +3,14 @@ import "./styles.scss";
 
 export interface IProps {
     username: string;
+    status?: string;
 }
 
 export default function User(props: IProps) {
     return (
         <>
             <div className="user">
-                <RoundIcon status="idle" small disableHover imageURL="https://external-preview.redd.it/4PE-nlL_PdMD5PrFNLnjurHQ1QKPnCvg368LTDnfM-M.png?auto=webp&s=ff4c3fbc1cce1a1856cff36b5d2a40a6d02cc1c3" />
+                <RoundIcon status={props.status || 'offline'} small disableHover imageURL="https://external-preview.redd.it/4PE-nlL_PdMD5PrFNLnjurHQ1QKPnCvg368LTDnfM-M.png?auto=webp&s=ff4c3fbc1cce1a1856cff36b5d2a40a6d02cc1c3" />
                 <div className="user__infos">
                     <span className="user__name">{props.username}</span>
                 </div>
