b785a224155f845561d88f04a6077334797cfceb
justheimsk <vonderheimsk@gmail.com>
2024-06-09 21:14:12 +0000

    feat: send message delete request

--- client/src/components/Message/index.tsx
+++ client/src/components/Message/index.tsx
@@ -2,12 +2,13 @@ import { FaTrash } from "react-icons/fa";
 import LibMessage from "../../lib/core/classes/Message";
 import RoundIcon from "../RoundIcon";
 import "./styles.scss";
+import Modal from "../Modal";
+import { useState } from "react";
 
 interface IProps {
-    content: string;
-    username: string;
-    attached?: LibMessage[]
+    self: LibMessage
     author?: boolean;
+    onDelete?: (msg: LibMessage) => void;
 }
 
 export default function Message(props: IProps) {
@@ -16,11 +17,11 @@ export default function Message(props: IProps) {
             <div className={`message ${props.author != true ? 'message--no-author' : ''}`}>
                 {props.author == true && <RoundIcon medium disableHover imageURL="https://external-preview.redd.it/4PE-nlL_PdMD5PrFNLnjurHQ1QKPnCvg368LTDnfM-M.png?auto=webp&s=ff4c3fbc1cce1a1856cff36b5d2a40a6d02cc1c3" />}
                 <div className="message__data">
-                    {props.author == true && <span className="message__author">{props.username}</span>}
+                    {props.author == true && <span className="message__author">{props.self.author.user.username}</span>}
                     <div className="message__content">
-                        <span>{props.content}</span>
+                        <span>{props.self.content}</span>
                         <div className="message__actions">
-                            <i className="message__action message__action--red"><FaTrash /></i>
+                            <div onClick={() => props.onDelete?.(props.self)} className="message__action message__action--red"><i><FaTrash /></i></div>
                         </div>
                     </div>
                 </div>
