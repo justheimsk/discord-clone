7f64c7c9add4d4a2931305bb18978fe826f4f5bb
justheimsk <vonderheimsk@gmail.com>
2024-06-09 14:25:25 +0000

    feat: better message stacking

--- client/src/components/Message/index.tsx
+++ client/src/components/Message/index.tsx
@@ -6,19 +6,17 @@ interface IProps {
     content: string;
     username: string;
     attached?: LibMessage[]
+    author?: boolean;
 }
 
 export default function Message(props: IProps) {
     return (
         <>
-            <div className="message">
-                <RoundIcon medium disableHover imageURL="https://external-preview.redd.it/4PE-nlL_PdMD5PrFNLnjurHQ1QKPnCvg368LTDnfM-M.png?auto=webp&s=ff4c3fbc1cce1a1856cff36b5d2a40a6d02cc1c3" />
+            <div className={`message ${props.author != true ? 'message--no-author' : ''}`}>
+                {props.author == true && <RoundIcon medium disableHover imageURL="https://external-preview.redd.it/4PE-nlL_PdMD5PrFNLnjurHQ1QKPnCvg368LTDnfM-M.png?auto=webp&s=ff4c3fbc1cce1a1856cff36b5d2a40a6d02cc1c3" />}
                 <div className="message__data">
-                    <span className="message__author">{props.username}</span>
-                    {!props.attached?.length && <span className="message__content">{props.content}</span>}
-                    {props.attached?.map((msg) => (
-                        <span className="message__content">{msg.content}</span>
-                    ))}
+                    {props.author == true && <span className="message__author">{props.username}</span>}
+                    <span className="message__content">{props.content}</span>
                 </div>
             </div>
         </>
