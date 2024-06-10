2cc13bb0e55e1441d0b0d13647de4c0e9130c975
justheimsk <vonderheimsk@gmail.com>
2024-06-10 14:30:22 +0000

    refactor: better message actions menu position

--- client/src/components/Message/index.tsx
+++ client/src/components/Message/index.tsx
@@ -18,12 +18,10 @@ export default function Message(props: IProps) {
                 {props.author == true && <RoundIcon medium disableHover imageURL="https://external-preview.redd.it/4PE-nlL_PdMD5PrFNLnjurHQ1QKPnCvg368LTDnfM-M.png?auto=webp&s=ff4c3fbc1cce1a1856cff36b5d2a40a6d02cc1c3" />}
                 <div className="message__data">
                     {props.author == true && <span className="message__author">{props.self.author.user.username}</span>}
-                    <div className="message__content">
-                        <span>{props.self.content}</span>
-                        <div className="message__actions">
-                            <div onClick={() => props.onDelete?.(props.self)} className="message__action message__action--red"><i><FaTrash /></i></div>
-                        </div>
-                    </div>
+                    <div className="message__content">{props.self.content}</div>
+                </div>
+                <div className="message__actions">
+                    <div onClick={() => props.onDelete?.(props.self)} className="message__action message__action--red"><i><FaTrash /></i></div>
                 </div>
             </div>
         </>
