8bb41ef3c5d3aa781ec10e27e7b2a6d9eee16b14
justheimsk <vonderheimsk@gmail.com>
2024-06-28 15:05:36 +0000

    refactor: change auth language to english

--- client/src/pages/Auth.tsx
+++ client/src/pages/Auth.tsx
@@ -72,42 +72,42 @@ export default function AuthPage(props: IProps) {
                 <div id="auth__layout">
                     <div id="auth__column--first" className="auth__column">
                         <div id="auth__text">
-                            <h2 id="auth__title">{props.method === "register" ? "Bem vindo" : "Bem vindo de volta!"}</h2>
-                            <h4 id="auth__subtitle">{props.method === "register" ? "Estamos ansiosos para você se juntar a nós!" : "Estamos felizes em ver você novamente"}</h4>
+                            <h2 id="auth__title">{props.method === "register" ? "Welcome!" : "Welcome back!"}</h2>
+                            <h4 id="auth__subtitle">{props.method === "register" ? "We look forward to you joining us!" : "We are happy to see you again"}</h4>
                         </div>
                         <form onSubmit={(e) => props.method === "register" ? register(e) : login(e)} action="" id="auth__form">
                             {props.method === "register" ? (
                                 <div className="auth__input-control">
-                                    <label htmlFor="username" className="auth__input-label">Nome de usuario</label>
+                                    <label htmlFor="username" className="auth__input-label">Username</label>
                                     <input onChange={(e) => setUsername(e.target.value)} required name='username' type="text" className="auth__input" />
-                                    <small id="auth__username-error" className="auth__input-error">O nome de usuario é invalido.</small>
+                                    <small id="auth__username-error" className="auth__input-error">Username invalid.</small>
                                 </div>
                             ) : ""}
                             <div className="auth__input-control">
                                 <label htmlFor="email" className="auth__input-label">Email</label>
                                 <input onChange={(e) => setEmail(e.target.value)} required name='email' type="email" className="auth__input" />
-                                <small id="auth__email-error" className="auth__input-error">O email é invalido.</small>
+                                <small id="auth__email-error" className="auth__input-error">Email invalid.</small>
                             </div>
                             <div className="auth__input-control">
-                                <label htmlFor="password" className="auth__input-label">Senha</label>
+                                <label htmlFor="password" className="auth__input-label">Password</label>
                                 <input onChange={(e) => setPassword(e.target.value)} type="password" required name='password' className="auth__input" />
-                                <small id="auth__password-error" className="auth__input-error">A senha é invalida.</small>
+                                <small id="auth__password-error" className="auth__input-error">Password invalid.</small>
                             </div>
 
-                            {props.method === "register" ? "" : <span className="auth__blue-deco">Esqueceu sua senha?</span>}
-                            <Button loading={loading} htmlType='submit' label={props.method === "register" ? "Criar conta" : "Entrar"} />
+                            {props.method === "register" ? "" : <span className="auth__blue-deco">Forgot your password?</span>}
+                            <Button loading={loading} htmlType='submit' label={props.method === "register" ? "Create account" : "Login"} />
                         </form>
                         {props.method === "register" ? (
-                            <span id="auth__change-method">Já tem uma conta? <Link to={'/login'} className="auth__blue-deco">Entre nela!</Link></span>
+                            <span id="auth__change-method">Already have an account? <Link to={'/login'} className="auth__blue-deco">Sign in!</Link></span>
                         ) : (
-                            <span id="auth__change-method">Não tem uma conta? <Link to={'/register'} className="auth__blue-deco">Crie uma!</Link></span>
+                            <span id="auth__change-method">Don't have an account? <Link to={'/register'} className="auth__blue-deco">Create one!</Link></span>
                         )}
                     </div>
                     <div className="auth__column">
                         <div id="auth__disclaimer">
                             <img src='github.png' width={176} height={176} alt="" />
-                            <span id="auth__disclaimer__text">Esse é um projeto feito por um fã do Discord!</span>
-                            <a target='_blank' rel="noreferrer" href='https://github.com/devdimer/discord-clone' id="auth__repo-link" className="auth__blue-deco">Repositorio no Github</a>
+                            <span id="auth__disclaimer__text">This is a project made by a Discord fan!</span>
+                            <a target='_blank' rel="noreferrer" href='https://github.com/devdimer/discord-clone' id="auth__repo-link" className="auth__blue-deco">Repository on Github</a>
                         </div>
                     </div>
                 </div>
